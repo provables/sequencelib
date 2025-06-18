91d45b45dc02725b449d49c1edd34dddcbfef56e
Walter Moreira <walter@waltermoreira.net>
2025-06-18 16:44:51 +0100

    Add interpretation of the triangular numbers

--- Sequencelib/Triangular.lean
+++ Sequencelib/Triangular.lean
@@ -28,3 +28,18 @@ theorem Triangular_two : Triangular 2 = 3 := rfl
 theorem Triangular_three : Triangular 3 = 6 := rfl
 theorem Triangular_four : Triangular 4 = 10 := rfl
 theorem Triangular_five : Triangular 5 = 15 := rfl
+
+/--
+Triangular numbers from the binomial coefficient:
+$$
+\mathrm{Triangular}(n) = \binom{n+1}{2}
+$$
+-/
+@[OEIS := A000217]
+def TriangularChooseTwo (n : ℕ) : ℕ := (n + 1).choose 2
+
+theorem Triangular_eq_TriangularChooseTwo : Triangular = TriangularChooseTwo := by
+  ext n
+  simp [Triangular, TriangularChooseTwo, Nat.choose_two_right (n + 1)]
+  congr 1
+  ring

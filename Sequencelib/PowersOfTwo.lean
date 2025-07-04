60b4ca1be3c4a6fca7f335839f4b72b378973d30
Walter Moreira <walter@waltermoreira.net>
2025-07-03 19:42:52 -0500

    Add comment and header

--- Sequencelib/PowersOfTwo.lean
+++ Sequencelib/PowersOfTwo.lean
@@ -1,7 +1,15 @@
+/-
+Copyright (c) 2025 Walter Moreira, Joe Stubbs. All rights reserved.
+Released under CC BY-SA 4.0 license as described in the file LICENSE.
+Authors: Walter Moreira, Joe Stubbs
+-/
 import Mathlib
 import Sequencelib.Meta
 
 namespace Sequence
 
+/-!
+Powers of 2: `a(n) = 2^n`.
+-/
 @[OEIS := A000079, maxIndex := 10, derive := true]
 def PowersOfTwo (n : ℕ) : ℕ := 2 ^ n

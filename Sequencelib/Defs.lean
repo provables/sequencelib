3e043dc6386bff45bd62745dd5d630c123e42e2f
Walter Moreira <walter@waltermoreira.net>
2025-07-16 15:25:24 -0500

    Add copyright comment

--- Sequencelib/Defs.lean
+++ Sequencelib/Defs.lean
@@ -1,1 +1,6 @@
+/-
+Copyright (c) 2025 Walter Moreira, Joe Stubbs. All rights reserved.
+Released under CC BY-SA 4.0 license as described in the file LICENSE.
+Authors: Walter Moreira, Joe Stubbs
+-/
 import Sequencelib.Defs.Triangle

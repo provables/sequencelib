4a00f2df89303cb372dcc552b93340f7359ef50d
Walter Moreira <walter@waltermoreira.net>
2025-07-16 00:12:35 -0500

    Prove equivalence of "imperative" code

--- Sequencelib/Triangular.lean
+++ Sequencelib/Triangular.lean
@@ -37,6 +37,33 @@ def Triangular2 (n : ℕ) : ℕ := Id.run do
     y := y + 1
   result
 
+theorem Triangular2_eq_Triangular : Triangular2 = Triangular := by
+  ext n
+  simp [Triangular2, Triangular]
+  reduce
+  generalize h : (fun (b : MProd ℕ (MProd ℕ ℕ)) (a : ℕ)
+    ↦ (⟨b.2.1, ⟨(b.2.1.add b.2.2).succ, b.2.2.succ⟩⟩ : MProd ℕ (MProd ℕ ℕ))) = g at *
+  dsimp at h ⊢
+  suffices h : List.foldl g ⟨0, 1, 1⟩ (List.range' 1 n 1) =
+      ⟨(n * n + n) / 2, (n * n + n) / 2 + n + 1, n + 1⟩ by
+    rw [h]
+    reduce
+    rfl
+  induction n with
+  | zero => simp
+  | succ n ih =>
+    rw [List.range'_1_concat, List.foldl_concat, ih, ← h]
+    simp
+    refine Eq.symm (Nat.div_eq_of_eq_mul_right (by linarith) ?_)
+    rw [mul_add (a := 2), mul_add (a := 2), Nat.mul_div_cancel']
+    linarith
+    rw [(show n * n + n = n * (n + 1) by linarith)]
+    by_cases he : Even n
+    · refine Nat.dvd_mul_right_of_dvd (even_iff_two_dvd.mp he) (n + 1)
+    · refine Nat.dvd_mul_left_of_dvd ?_ n
+      rw [← Nat.even_add_one] at he
+      exact even_iff_two_dvd.mp he
+
 theorem Triangular_zero : Triangular 0 = 0 := rfl
 theorem Triangular_one : Triangular 1 = 1 := rfl
 theorem Triangular_two : Triangular 2 = 3 := rfl

689016f319d6697df609f9cfb9c5ab0eac0bb7e5
Walter Moreira <walter@waltermoreira.net>
2025-09-18 16:26:28 -0500

    Fixing docker issues

--- flake.nix
+++ flake.nix
@@ -3,17 +3,19 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
     shell-utils.url = "github:waltermoreira/shell-utils";
-    taskdep.url = "github:waltermoreira/taskdep";
     synthetic.url = "github:provables/synthetic";
     lean-toolchain.url = "github:provables/lean-toolchain-nix";
+    nix-docker-img.url = "github:provables/nix-docker-img";
   };
-  outputs = { self, nixpkgs, flake-utils, shell-utils, synthetic, taskdep, lean-toolchain }:
+  outputs = { self, nixpkgs, flake-utils, shell-utils, synthetic, lean-toolchain, nix-docker-img }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         shell = shell-utils.myShell.${system};
         toolchain = lean-toolchain.packages.${system}.lean-toolchain-4_20;
         genseq = synthetic.packages.${system}.default;
+        supervisedGenseq = synthetic.packages.${system}.supervisedGenseq;
+        buildNixImage = nix-docker-img.lib.${system}.buildNixImage;
         blueprints = pkgs.python311.pkgs.buildPythonPackage {
           name = "blueprints";
           src = pkgs.fetchFromGitHub {
@@ -70,9 +72,8 @@
           which
           file
           procps
-          taskdep.packages.${system}.default
         ] ++ lib.optional stdenv.isDarwin apple-sdk_14
-        ++ lib.optional stdenv.isLinux strace ;
+        ++ lib.optional stdenv.isLinux strace;
         devEnvPackages = with pkgs; [
           texliveFull
           ghostscript
@@ -98,27 +99,30 @@
           '';
         };
 
-      app = pkgs.writeShellApplication {
-        name = "synthesize";
-        runtimeInputs = [ toolchain pkgs.git pkgs.rsync ];
-        text = ''
-          mkdir -p "$TMP"/sequencelib
-          cd "$TMP"/sequencelib
-          rsync -a ${./.}/ .
-          chmod -R +w "$TMP"/sequencelib
-          lake exe cache get
-          ${python}/bin/python -u ./scripts/synthesize.py "$@"
-          echo "Output at: $TMP/sequencelib/Sequencelib/Synthetic" | \
-            ${pkgs.boxes}/bin/boxes -d shell -p h2v1
-        '';
-      };
-    
-      dockerImage = pkgs.dockerTools.buildLayeredImage  {
+        app = pkgs.writeShellApplication {
+          name = "synthesize";
+          runtimeInputs = [ toolchain pkgs.git pkgs.rsync ];
+          text = ''
+            TMP=$(mktemp -d)
+            export TMP
+            mkdir -p "$TMP"/sequencelib
+            cd "$TMP"/sequencelib
+            rsync -r ${./.}/ .
+            chmod -R +w "$TMP"/sequencelib
+            lake exe cache get
+            ${python}/bin/python -u ./scripts/synthesize.py "$@"
+            echo "Output at: $TMP/sequencelib/Sequencelib/Synthetic" | \
+              ${pkgs.boxes}/bin/boxes -d shell -p h2v1
+          '';
+        };
+
+        dockerImage = buildNixImage {
           name = "provables/synthesize";
           tag = "latest";
           contents = [
             basePackages
             genseq
+            supervisedGenseq
             app
             scripts
           ];

4510600fc3a842c500aad3063f85fb7cb37723e9
Walter Moreira <walter@waltermoreira.net>
2026-03-01 23:20:29 -0600

    Update schema

--- sequencelib.sql
+++ sequencelib.sql
@@ -1,47 +1,43 @@
 BEGIN TRANSACTION;
-DROP TABLE IF EXISTS "declaration";
-CREATE TABLE "declaration" (
+CREATE TABLE IF NOT EXISTS "declaration" (
 	"module"	TEXT NOT NULL,
 	"name"	TEXT NOT NULL,
 	"computable"	INTEGER NOT NULL,
-	"tag"	INTEGER,
+	"tag"	INTEGER NOT NULL UNIQUE,
 	"declaration_id"	INTEGER NOT NULL,
 	"sequence_id"	INTEGER NOT NULL,
 	PRIMARY KEY("declaration_id"),
+	UNIQUE("module","name"),
 	FOREIGN KEY("sequence_id") REFERENCES "sequence"("sequence_id")
 );
-DROP TABLE IF EXISTS "keyword";
-CREATE TABLE "keyword" (
+CREATE TABLE IF NOT EXISTS "keyword" (
 	"keyword_id"	INTEGER NOT NULL,
 	"keyword"	TEXT NOT NULL,
 	"description"	TEXT,
+	"type"	INTEGER NOT NULL DEFAULT 0,
 	PRIMARY KEY("keyword_id")
 );
-DROP TABLE IF EXISTS "sequence";
-CREATE TABLE "sequence" (
-	"tag"	INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS "sequence" (
+	"tag"	INTEGER NOT NULL UNIQUE,
 	"description"	TEXT NOT NULL,
 	"offset"	INTEGER NOT NULL,
 	"codomain"	INTEGER NOT NULL DEFAULT 0,
 	"sequence_id"	INTEGER NOT NULL,
 	PRIMARY KEY("sequence_id")
 );
-DROP TABLE IF EXISTS "sequence_keyword";
-CREATE TABLE "sequence_keyword" (
+CREATE TABLE IF NOT EXISTS "sequence_keyword" (
 	"sequence_id"	INTEGER NOT NULL,
 	"keyword_id"	INTEGER NOT NULL,
 	FOREIGN KEY("keyword_id") REFERENCES "keyword"("keyword_id"),
 	FOREIGN KEY("sequence_id") REFERENCES "sequence"("sequence_id")
 );
-DROP TABLE IF EXISTS "sequence_value";
-CREATE TABLE "sequence_value" (
+CREATE TABLE IF NOT EXISTS "sequence_value" (
 	"sequence_id"	INTEGER NOT NULL,
 	"index"	INTEGER NOT NULL,
 	"value"	INTEGER NOT NULL,
 	FOREIGN KEY("sequence_id") REFERENCES "sequence"("sequence_id")
 );
-DROP TABLE IF EXISTS "theorem_equivalence";
-CREATE TABLE "theorem_equivalence" (
+CREATE TABLE IF NOT EXISTS "theorem_equivalence" (
 	"declaration_left_id"	INTEGER NOT NULL,
 	"declaration_right_id"	INTEGER NOT NULL,
 	"name"	TEXT NOT NULL,
@@ -49,8 +45,7 @@ CREATE TABLE "theorem_equivalence" (
 	FOREIGN KEY("declaration_left_id") REFERENCES "declaration"("declaration_id"),
 	FOREIGN KEY("declaration_right_id") REFERENCES "declaration"("declaration_id")
 );
-DROP TABLE IF EXISTS "theorem_value";
-CREATE TABLE "theorem_value" (
+CREATE TABLE IF NOT EXISTS "theorem_value" (
 	"declaration_id"	INTEGER NOT NULL,
 	"index"	INTEGER NOT NULL,
 	"value"	INTEGER NOT NULL,

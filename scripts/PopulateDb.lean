6ad827c096857c0b2e8881dace57e5a41a7ab1ab
Walter Moreira <walter@waltermoreira.net>
2026-03-31 00:11:30 -0500

    Add cli scaffolding

--- scripts/PopulateDb.lean
+++ scripts/PopulateDb.lean
@@ -1,11 +1,33 @@
+import Cli.Basic
 import Sequencelib.Meta.PopulateDb
 
-open Lean Elab Meta
+open Lean Elab Meta Cli
+
+def VERSION := "0.1.0"
 
 unsafe
-def main : IO Unit := do
+def run (_ : Parsed) : IO UInt32 := do
   let modules := #[`Sequencelib.Meta.PopulateDb]
   enableInitializersExecution
   initSearchPath (← findSysroot)
   let env ← importModules (modules.map ({module := ·})) {} (trustLevel := 1024) (loadExts := true)
-  DbM.fromEnvToIO <| populateDb env default "/Users/walter/repos/oeisdata"
+  let some oeisdata ← IO.getEnv "SEQUENCELIB_OEISDATA" |
+    throw <| .userError "missing `SEQUENCELIB_OEISDATA` environment variable"
+  DbM.fromEnvToIO <| populateDb env default oeisdata
+  return 0
+
+unsafe
+def cmd : Cmd := `[Cli|
+  "populatedb" VIA run; [VERSION]
+  "Populate Sequencelib Database.
+
+  Environment variables:
+
+  SEQUENCELIB_SCHEMA:   database schema [default: ./sequencelib.sql]
+  SEQUENCELIB_DATABASE: database file   [default: ./sequencelib.db]
+  SEQUENCELIB_OEISDATA: path to OEIS repository"
+]
+
+unsafe
+def main (args : List String) : IO UInt32 := do
+  cmd.validate args

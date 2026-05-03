3b19b36a80951d08fd8d71f012a92184ced8cefd
Walter Moreira <walter@waltermoreira.net>
2026-05-02 19:04:01 -0500

    Pass modules to load via parameters

--- scripts/PopulateDb.lean
+++ scripts/PopulateDb.lean
@@ -1,19 +1,31 @@
 import Cli.Basic
 import Sequencelib.Meta.PopulateDb
+import Sequencelib.Meta.OEISInfo
 
 open Lean Elab Meta Cli
 
-def VERSION := "0.1.0"
+def VERSION := "0.2.0"
+
+def getModules (mods : String) : Array Name :=
+  if mods.trimAscii == "" then
+    #[]
+  else
+    mods.split ',' |>.map (·.trimAscii.toName) |>.toArray
 
 unsafe
-def run (_ : Parsed) : IO UInt32 := do
+def run (p : Parsed) : IO UInt32 := do
   let modules := #[`Sequencelib.Meta.PopulateDb]
+    ++ (p.flag? "modules" |>.map (·.as! String) |>.map getModules |>.getD #[])
+  --let modules := #[`Sequencelib.Meta.PopulateDb, `Sequencelib.FiniteGroups]
   enableInitializersExecution
   initSearchPath (← findSysroot)
   let env ← importModules (modules.map ({module := ·})) {} (trustLevel := 1024) (loadExts := true)
   let some oeisdata ← IO.getEnv "SEQUENCELIB_OEISDATA" |
     throw <| .userError "missing `SEQUENCELIB_OEISDATA` environment variable"
-  let tags ← DbM.fromEnvToIO <| populateDb env default oeisdata
+  let ctx := {fileName := default, fileMap := default}
+  let state := {env}
+  let tagsWithInfo ← Prod.fst <$> (Meta.MetaM.toIO · ctx state) getTagsWithInfo
+  let tags ← DbM.fromEnvToIO <| populateDb env tagsWithInfo oeisdata
   IO.println s!"Ok: {tags.size} entries processed."
   return 0
 
@@ -27,6 +39,9 @@ def cmd : Cmd := `[Cli|
   SEQUENCELIB_SCHEMA:   database schema [default: ./sequencelib.sql]
   SEQUENCELIB_DATABASE: database file   [default: ./sequencelib.db]
   SEQUENCELIB_OEISDATA: path to OEIS repository"
+
+  FLAGS:
+    m, modules : String;        "List of modules to process (comma separated)"
 ]
 
 unsafe
